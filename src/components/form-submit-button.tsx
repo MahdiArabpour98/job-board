ad4b62c000ff078290fb881b1757713974e7fe44
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-01-16 18:46:41 +0330

    approved and delete functions and approved button

--- src/components/form-submit-button.tsx
+++ src/components/form-submit-button.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useFormStatus } from "react-dom";
-import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
 import LoadingButton from "@/components/loading-button";
 
 const FormSubmitButton = (
